3a8679fc798aaa7341287754264e391ce6b7e146
SecretFr <linkclean@naver.com>
2021-08-27 03:31:37 +0900

    Arrows Function

--- es6/Arrows.js
+++ es6/Arrows.js
@@ -1,4 +1,34 @@
-var even=[2,4,6,8];
+var evens=[2,4,6,8];
+var fives = [];
 
-var odds = even.map(v => v+1);
-console.log(odds);
+//Expression bodies
+var odds = evens.map(v => v+1);
+var nums = evens.map((v, i) => v+i);
+var pairs = evens.map(v => ({even: v, odd: v+1}));
+
+console.log("map odds : "+odds);
+console.log("v + i: "+nums);
+
+//Statement bodies
+pairs.forEach(v => {
+   console.log(v);
+});
+
+//Statement bodies
+nums.forEach(v => {
+    if(v%5 === 0){
+        fives.push(v);
+    }
+});
+console.log(fives);
+
+//Lexical this
+var bob = {
+    _name: "Bob",
+    _friends: ["John, Brian"],
+    printFriends(){
+        this._friends.forEach(f =>
+            console.log(this._name + " knows " + f));
+    }
+}
+console.log(bob.printFriends());
